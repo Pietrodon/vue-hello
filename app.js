064d015d31f3dbc7c7a6e3cc6817d7e8dc6f4348
Pietrodon <donvitopietro00@gmail.com>
2022-02-23 16:27:22 +0100

    stampa immagine

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let test = new Vue ({
     el : '#prova',
     data:{
-        message: 'Ciao a tutti,come va?'
+        message: 'Ciao a tutti,come va?',
+        img: 'https://picsum.photos/200/300'
     }
 })
